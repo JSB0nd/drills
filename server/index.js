ec1e0f6638695db39bcb5699301c292359f8b887
Kuzmin Anton <antofer@mail.ru>
2025-08-27 09:38:07 +0300

    4.8, 4.9

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const fs_promises = fs.promises;
 const path = require('path');
 const readJsonFiles = require('./read');
+const getPosts = require('./posts');
 
 const app = express();
 const PORT = process.env.PORT || 3037;
@@ -107,6 +108,14 @@ app.get('/api/v1/drills/file', (req, res) => {
 
 app.get('/api/v1/drills', (req, res) => {
   return res.status(200).json(readJsonFiles('data'));
+});
+
+app.get('/api/v1/posts', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  return res.status(200).json(getPosts());
 });
 
 app.listen(PORT, () => {
